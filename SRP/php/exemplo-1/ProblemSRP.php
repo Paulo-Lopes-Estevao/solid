fe4f035c872d4db72e7fb9f98443125e6a980fb6
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-08-11 09:49:09 +0100

    feat: created problem implemented SRP

---
+++ SRP/php/exemplo-1/ProblemSRP.php
@@ -0,0 +1,30 @@
+<?php
+
+class Bakery
+{
+
+    public function connect()
+    {
+        /***/
+    }
+
+    public function createBaker()
+    {
+        /***/
+    }
+
+    public function createAttendant()
+    {
+        /***/
+    }
+
+    public function attendanceRecord()
+    {
+        /***/
+    }
+
+    public function cashierClosing()
+    {
+        /***/
+    }
+}
